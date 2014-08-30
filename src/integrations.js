8a91117bae9ca1c9dc4027cb883b805d8dc838c7
Dustin Coates <dustin@dcoates.com>
2014-08-29 20:30:16 -0400

    Misc changes
    Rename trackPurchase, add mixpanel

--- src/integrations.js
+++ src/integrations.js
@@ -3,22 +3,29 @@ _integrationsMasterList['google-analytics'] = {
   trackPageView: function(){
     _gaq.push(['_trackPageview']);
   },
-  trackPurchase: function(){},
+  trackConversion: function(){
+
+  },
   trackAction: function(opts){
     _trackEvent(opts.category, opts.action, opts.label, opts.value, opts.nonInteraction);
   }
 };
-_integrationsMasterList['google'] = _integrationsMasterList['google-analytics'];
 _integrationsMasterList['ga'] = _integrationsMasterList['google-analytics'];
+_integrationsMasterList['google'] = _integrationsMasterList['google-analytics'];
 _integrationsMasterList['google-universal-analytics'] = {
   trackPageView: function(){},
-  trackPurchase: function(){},
+  trackConversion: function(){},
   trackAction: function(){}
 };
+_integrationsMasterList['universal-analytics'] = _integrationsMasterList['google-universal-analytics'];
 _integrationsMasterList['kissmetrics'] = {
   trackPageView: function(){},
-  trackPurchase: function(){},
+  trackConversion: function(){},
   trackAction: function(){}
 };
 _integrationsMasterList['km'] = _integrationsMasterList['kissmetrics'];
-_integrationsMasterList['universal-analytics'] = _integrationsMasterList['google-universal-analytics'];
+_integrationsMasterList['mixpanel'] = {
+  trackPageView: function(){},
+  trackConversion: function(){},
+  trackAction: function(){}
+};
